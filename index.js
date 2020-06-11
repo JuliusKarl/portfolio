45ba5ceb3fad56b798c8dd210432472c20e4dba1
Julius Karl Macrohon <julius.karl.macrohon@gmail.com>
2020-06-12 01:44:02 +1200

    Text refactor

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     var prevScrollpos = window.pageYOffset;
     window.onscroll = function() {
         var currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos || currentScrollPos == 0) {
+        if (prevScrollpos >= currentScrollPos) {
             document.getElementById("navbar").style.top = "0";
         } else {
             document.getElementById("navbar").style.top = "-100px";
