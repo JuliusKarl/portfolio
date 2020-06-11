b32e76837ec92962a6709bf00ac6a53ce3bcd65f
Julius Karl Macrohon <julius.karl.macrohon@gmail.com>
2020-06-12 01:26:57 +1200

    Social Icons navbar height fixed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     var prevScrollpos = window.pageYOffset;
     window.onscroll = function() {
         var currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos || prevScrollpos == currentScrollPos) {
+        if (prevScrollpos > currentScrollPos || currentScrollPos == 0) {
             document.getElementById("navbar").style.top = "0";
         } else {
             document.getElementById("navbar").style.top = "-100px";
