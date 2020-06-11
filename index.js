540db519d546655fcf561160352c2701ba6cb058
Julius Karl Macrohon <julius.karl.macrohon@gmail.com>
2020-06-12 00:29:04 +1200

    refactor

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ $(document).ready(function() {
     // Navbar hide
     var prevScrollpos = window.pageYOffset;
     window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-100px";
-    }
-    prevScrollpos = currentScrollPos;
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-100px";
+        }
+        prevScrollpos = currentScrollPos;
     }
 
     // Vanta
